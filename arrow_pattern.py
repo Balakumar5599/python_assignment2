c4a0397f1282950475cb7465d9420733f1bb07d2
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-06-11 19:35:15 +0530

    function method is added

--- arrow_pattern.py
+++ arrow_pattern.py
@@ -1,5 +1,7 @@
 # 2. Get the total number of rows to be printed from the user input and print the pattern:-
 
+#Method-1
+
 num_of_row=int(input("Enter the number of rows: "))
 
 for row in range(0,num_of_row):
@@ -15,3 +17,27 @@ for row in range(0,num_of_row):
         print("*"*(ref_num-1)+"*"*(ref_num-2),end="")
         print()
         ref_num-=1
+        
+        
+#Method-2 Using function
+
+def arrow_pattern(num_of_row):
+    
+    for row in range(0,num_of_row):
+    
+        if row<=num_of_row//2:
+        
+            print("*"*(row+1)+"*"*(row),end="")
+            print()
+            ref_num=row+1
+        
+        if row>num_of_row//2:
+            
+            print("*"*(ref_num-1)+"*"*(ref_num-2),end="")
+            print()
+            ref_num-=1
+
+num_of_row=int(input("Enter the number of rows: "))
+arrow_pattern(num_of_row)
+
+    
