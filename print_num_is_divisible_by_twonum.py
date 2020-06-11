be3bfe740ff20c47e9974d394f3ec02a0cecc302
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-06-11 19:27:03 +0530

    using function method is added

--- print_num_is_divisible_by_twonum.py
+++ print_num_is_divisible_by_twonum.py
@@ -7,3 +7,16 @@ print("The Numbers that are completely divisible for the given range are: ")
 for value in range(range_from,range_to+1):
     if value%75==0 and value%100==0:
         print(value)
+
+        
+# Using function
+
+def divisible_num(range_from,range_to):
+    print("The Numbers that are completely divisible for the given range are: ")
+    for value in range(range_from,range_to+1):
+        if value%75==0 and value%100==0:
+            print(value)
+
+range_from=int(input("Enter the range value from: "))
+range_to=int(input("Enter the range value to: "))
+divisible_num(range_from,range_to)
